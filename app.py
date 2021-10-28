b29baf729d81894268d90e6d8c42d90be4aff7ef
gsukumar2 <92962329+gsukumar2@users.noreply.github.com>
2021-10-28 16:59:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def index():
    
     
     url = random.choice(images)
-    return render_template('index.html', url=url, company = response.status_code)
+    return render_template('index.html', url=url)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
